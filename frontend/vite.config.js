c494ba86cd439e82103df68561b9d83eda8921ca
Ahtishamul Haque <112795299+ahtishamul-haque@users.noreply.github.com>
2025-06-24 18:55:46 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 8080,
     proxy: {
-      '/search': 'http://localhost:8080'
+      '/search': 'http://localhost:3000'
+    }
+  },
+  build: {
+    outDir: 'build',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
     }
   }
 });
